659ef86ea691dfdd0d463b2e14073d4c5e435536
rishijain2051-coder <rishi.jain2051@gmail.com>
2026-07-19 17:07:49 +0530

    Update FeaturedProducts.tsx

--- src/app/components/FeaturedProducts.tsx
+++ src/app/components/FeaturedProducts.tsx
@@ -203,7 +203,7 @@ export default function FeaturedProducts() {
 
         {/* Products Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
-          {products.map((product, i) =>
+          {products.slice(0, 6).map((product, i) =>
           <div
             key={product.id}
             className="fade-up group product-card-hover cursor-pointer"
